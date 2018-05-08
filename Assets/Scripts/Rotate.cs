f3adb129cece821c66e6720ffb766a66696529b0
zeivhann <alexsilcott@gmail.com>
2018-05-07 21:57:30 -0400

    Created initial project. Valve spins like mad.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    Transform bolt;
+
+    // Use this for initialization
+    void Start () {
+        bolt = GameObject.FindGameObjectWithTag("Pivot").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(bolt.position, Vector3.forward, 15f);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VR-Valve
-First VR project in Unity. Created with VRTK and Steam VR.
+First VR project in Unity. Created with VRTK and SteamVR.
+
+This project requires [VRTK](https://github.com/thestonefox/VRTK) and the [SteamVR plugin](https://assetstore.unity.com/packages/templates/systems/steamvr-plugin-32647) (Or whatever SDK for your hardware.).
